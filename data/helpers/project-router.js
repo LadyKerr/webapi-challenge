da00e45f62f0f67b94ca6a1a49d7d11bcc3b3371
LadyKerr <kadiekerr@gmail.com>
2019-07-12 13:04:20 -0400

    CRUD for projects & actions completed

--- data/helpers/project-router.js
+++ data/helpers/project-router.js
@@ -5,7 +5,7 @@ const Projects = require("./projectModel");
 
 const router = express.Router();
 
-//display actions
+//display projects
 router.get("/", (req, res) => {
   Projects.get()
     .then(project => {
@@ -13,8 +13,7 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        message:
-          "There was an error retrieving projects from the database. Please try again."
+        error: "There was an error retrieving projects from the database."
       });
     });
 });
@@ -41,26 +40,115 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// router.post("/:id/actions", (req, res) => {
-//   let newAction = req.body;
-//   const { description, notes } = req.body;
-//   newAction.project_id = id;
+//get project actions
+router.get("/:id/actions", (req, res) => {
+  const { id } = req.params;
 
-//   if (!description || !notes) {
-//     res.status(400).json({
-//       message: "Please provide a description and notes for the action"
-//     });
-//   } else {
-//     Actions.insert(newAction)
-//       .then(newAction => {
-//         res.status(201).json(newAction);
-//       })
-//       .catch(err => {
-//         res.status(500).json({
-//           message: "There was an error adding this action to the database."
-//         });
-//       });
-//   }
-// });
+  Projects.getProjectActions(id)
+    .then(projectAction => {
+      if (projectAction && projectAction.length) {
+        res.status(200).json(projectAction);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The project with that ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error getting the requested project actions."
+      });
+    });
+});
+
+//delete projects
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Projects.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(200).json(deleted);
+      } else {
+        res.status(404).json({
+          message: "The project with that ID is not in the database."
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The project could not be deleted." });
+    });
+});
+
+//add new project
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name || !description) {
+    res.status(400).json({
+      errorMessage: "Please provide a name and description for the project."
+    });
+  } else {
+    Projects.insert(req.body)
+      .then(newProject => {
+        res.status(200).json(newProject);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The project could not be added. Please try again" });
+      });
+  }
+});
+
+//add new action to the specified project
+router.post("/:id/actions", (req, res) => {
+  let newAction = req.body;
+  const { description, notes } = req.body;
+  const { id } = req.params;
+  newAction.project_id = id;
+
+  if (!description || !notes) {
+    res.status(400).json({
+      message: "Please provide a description and notes for the action"
+    });
+  } else {
+    Actions.insert(newAction)
+      .then(newAction => {
+        res.status(201).json(newAction);
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error adding this action to the database."
+        });
+      });
+  }
+});
+
+//update project
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  const { description, name } = req.body;
+
+  Projects.update(id, changes)
+    .then(updated => {
+      if (!updated) {
+        res
+          .status(404)
+          .json({ message: "The project with that ID does not exist." });
+      } else if (!description || !name) {
+        res.status(400).json({
+          errorMessage: "Please provide description and name for the project."
+        });
+      } else {
+        res.status(200).json(updated);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The project could not be modified." });
+    });
+});
 
 module.exports = router;
