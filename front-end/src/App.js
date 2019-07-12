e90f7ba5f18ff174aaa5059e2ddfdb600673a248
LadyKerr <kadiekerr@gmail.com>
2019-07-12 13:37:27 -0400

    started working on stretch

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from App!</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    // actions: [],
+    projects: []
+  };
+
+  //grab projects from DB
+  componentDidMount() {
+    axios
+      .get("http://localhost:8000/api/projects")
+      .then(res => {
+        this.setState({ projects: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from App!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
