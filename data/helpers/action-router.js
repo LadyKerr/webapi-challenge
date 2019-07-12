bd031893d71a3a0c8fd881705e8472276d911e29
LadyKerr <kadiekerr@gmail.com>
2019-07-12 11:40:28 -0400

    GET requests for actions

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -5,4 +5,40 @@ const Projects = require("./projectModel");
 
 const router = express.Router();
 
+//display actions
+router.get("/", (req, res) => {
+  Actions.get()
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          "There was an error retrieving actions from the database. Please try again."
+      });
+    });
+});
+
+//display actions by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The action with that ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          "There was an error retrieving actions from the database. Please try again."
+      });
+    });
+});
+
 module.exports = router;
